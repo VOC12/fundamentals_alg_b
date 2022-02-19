6a281298409d151a512a1bd4db435882f998fd72
VOC12 <2109155@upy.edu.mx>
2022-02-19 15:48:39 -0600

    created some vafriables

---
+++ hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hellow World. \n");
+	printf("Cynthia Vidal. \n");
+	return 0;
+}
---
+++ saludo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	for(;;){
+		printf("Hellow World...\n");
+	}
+	return 0;
+}
