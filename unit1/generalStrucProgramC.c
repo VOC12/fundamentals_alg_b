4768eec0baa69b060967f77d83352413a6491db0
VOC12 <2109155@upy.edu.mx>
2022-03-18 07:59:04 -0600

    .

--- unit1/generalStrucProgramC.c
+++ unit1/generalStrucProgramC.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
+
 //* Global Variables *//;
 //Create a variable: dataType nameVariable = anyData;
-//dataTypes in C/C++/C#/PHP
+//dataTypes in C/C++/CH/PHP
 int unEntero = 3;
 float unDecimal = 3.1416;
-char* name = "Cynthia Vidal";
+char* name = "CynthiaVidal";
+char unCaracter = 'M';
+//////////////////////////////////
 
-char unCaracter = 'C';
-
+/// La ejecucucion del programa//
 int main(){
-
-    printf("Hello Data Engineer B %s\n", name);
+    printf("Hello Data B $s\n", name);
     return 0;
 }
